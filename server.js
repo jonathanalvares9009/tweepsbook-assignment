acca5b677b29c639a47ac4a58a46edba8e4638d4
Jonathan Alvares <alvaresjonathan599@gmail.com>
2022-10-14 14:43:46 +0530

    Updated for heroku

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const fastify = require('fastify')({ logger: true });
-const PORT = 5000;
+const PORT = 5000 || process.env.PORT;
 
 require('./connection/config.js')
 fastify.register(require('./routes/user.js'))
 
-fastify.listen(PORT, () => {
+fastify.listen(PORT, '0.0.0.0', () => {
     console.log("Server is runing on port http://localhost:5000");
 })
