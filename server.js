29b8acdff2e458fa3aba6a437e00ea3ef7e1072c
Jonathan Alvares <alvaresjonathan599@gmail.com>
2022-10-14 15:48:06 +0530

    Added default route

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const PORT = 5000 || process.env.PORT;
 require('./connection/config.js')
 fastify.register(require('./routes/user.js'))
 
+fastify.get("/", (req, res) => {
+    res.status(200).send("Server is working")
+})
+
 fastify.listen(PORT, process.env.HOST || '0.0.0.0', () => {
     console.log("Server is runing on port http://localhost:5000");
 })
