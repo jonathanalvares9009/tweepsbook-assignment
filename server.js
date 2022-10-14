d1a18ca792bf233020e7fcc24c083b58948a66e3
Jonathan Alvares <alvaresjonathan599@gmail.com>
2022-10-14 15:40:27 +0530

    Added host

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ const PORT = 5000 || process.env.PORT;
 require('./connection/config.js')
 fastify.register(require('./routes/user.js'))
 
-fastify.listen(PORT, '0.0.0.0', () => {
+fastify.listen(PORT, process.env.HOST || '0.0.0.0', () => {
     console.log("Server is runing on port http://localhost:5000");
 })
